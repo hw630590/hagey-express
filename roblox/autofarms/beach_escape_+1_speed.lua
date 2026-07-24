cc78b5ee2c88cdef42999d009280c2c4bb8dad6b
hw630590 <159571402+hw630590@users.noreply.github.com>
2026-07-24 23:49:24 +0100

    Create beach_escape_+1_speed.lua

---
+++ roblox/autofarms/beach_escape_+1_speed.lua
@@ -0,0 +1,31 @@
+-- Autofarm Steps!
+assert(getconnections, "[hageyexpress.lol] Missing getconnections")
+assert(debug and debug.getinfo, "[hageyexpress.lol] Missing debug.getinfo")
+
+local used = false
+
+local services = {
+    game:GetService("RunService").Heartbeat,
+    game:GetService("RunService").RenderStepped,
+}
+
+for _, service in ipairs(services) do
+    for _, conn in ipairs(getconnections(service)) do
+        local func = conn.Function
+        if func then
+            local info = debug.getinfo(func)
+            if info and info.source and info.source:match("Speed") then
+                conn:Disable()
+                used = true
+            end
+        end
+    end
+end
+
+print("[hageyexpress.lol] Bypassed Remote Spammer. Started Autofarm Now!")
+
+local duration = used and 0.01 or math.random(50, 80) / 1000
+
+while task.wait(duration) do
+    game:GetService("ReplicatedStorage").Events.AddSpeed:FireServer()
+end
