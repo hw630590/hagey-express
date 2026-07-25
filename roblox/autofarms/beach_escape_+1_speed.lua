e1dd57113877f30759d28c78c1d80d34df8a725e
hw630590 <159571402+hw630590@users.noreply.github.com>
2026-07-25 10:01:24 +0100

    Update beach_escape_+1_speed.lua

--- roblox/autofarms/beach_escape_+1_speed.lua
+++ roblox/autofarms/beach_escape_+1_speed.lua
@@ -1,4 +1,4 @@
-print("[hageyexpress.lol] DO NOT move for ~30 seconds while this script is running, doing so could result in you being kicked for remote spam. We are attempting to bypass this later on.")
+print("[hageyexpress.lol] You may move for ~30 seconds while this script is running, but it could result in you being kicked for remote spam. We are attempting to bypass this later on.")
 while task.wait(math.random(50, 80) / 1000) do
     game:GetService("ReplicatedStorage").Events.AddSpeed:FireServer()
 end
