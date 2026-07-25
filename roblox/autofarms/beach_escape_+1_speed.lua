57d756d13beaf55f0f8e7d748b59714f17b6b7c4
hw630590 <159571402+hw630590@users.noreply.github.com>
2026-07-25 10:00:21 +0100

    Update beach_escape_+1_speed.lua

--- roblox/autofarms/beach_escape_+1_speed.lua
+++ roblox/autofarms/beach_escape_+1_speed.lua
@@ -1,33 +1,4 @@
--- Autofarm Steps!
-assert(getconnections, "[hageyexpress.lol] Missing getconnections")
-assert(debug and debug.getinfo, "[hageyexpress.lol] Missing debug.getinfo")
-
-local used = false
-
-local services = {
-    game:GetService("RunService").Heartbeat,
-    game:GetService("RunService").RenderStepped,
-}
-
--- THIS MAY BE UNSTABLE, COMMENT OUT IF NECESSARY! ⌄⌄⌄⌄
-for _, service in ipairs(services) do
-    for _, conn in ipairs(getconnections(service)) do
-        local func = conn.Function
-        if func then
-            local info = debug.getinfo(func)
-            if info and info.source and info.source:match("Speed") then
-                conn:Disable()
-                used = true
-            end
-        end
-    end
-end
--- THIS MAY BE UNSTABLE, COMMENT OUT IF NECESSARY! ^^^^
-
-print("[hageyexpress.lol] Bypassed Anti-Remote Spam. Starting Autofarm Now!")
-
-local duration = used and 0.01 or math.random(50, 80) / 1000
-
-while task.wait(duration) do
+print("[hageyexpress.lol] DO NOT move for ~30 seconds while this script is running, doing so could result in you being kicked for remote spam. We are attempting to bypass this later on.")
+while task.wait(math.random(50, 80) / 1000) do
     game:GetService("ReplicatedStorage").Events.AddSpeed:FireServer()
 end
