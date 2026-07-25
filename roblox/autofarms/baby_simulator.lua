b7959c085a40a500808aa493aed30efefe9f703c
hw630590 <159571402+hw630590@users.noreply.github.com>
2026-07-25 21:09:22 +0100

    Update baby_simulator.lua

--- roblox/autofarms/baby_simulator.lua
+++ roblox/autofarms/baby_simulator.lua
@@ -1,4 +1,4 @@
--- the game developer is 'Broken Wand Studio' if the script doesnt work
+-- the game developer is 'Broken Wand Studios' btw
 if not game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Tool") then error("Equip A Tool!") end
 while task.wait() do
 game:GetService("ReplicatedStorage").Remotes.Input:FireServer()
